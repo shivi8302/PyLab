27bb2c949b67118a4cbd2d5140f9fdf976fb05cd
shivi <shivi07k@gmail.com>
2025-01-14 21:35:58 +0530

    turtle_panda

--- python_turtle1.py
+++ python_turtle1.py
@@ -13,52 +13,52 @@ def ring(col, rad):
 	pen.end_fill()
 
 pen.up()
-pen.setpos(-35, 95)
-pen.down
-ring('black', 15)
-
-pen.up()
-pen.setpos(35, 95)
+pen.goto(-35, 95)
 pen.down()
 ring('black', 15)
 
 pen.up()
-pen.setpos(0, 35)
+pen.goto(35, 95)
+pen.down()
+ring('black', 15)
+
+pen.up()
+pen.goto(0, 35)
 pen.down()
 ring('white', 40)
 
 pen.up()
-pen.setpos(-18, 75)
+pen.goto(-18, 75)
 pen.down
 ring('black', 8)
 
 pen.up()
-pen.setpos(18, 75)
+pen.goto(18, 75)
 pen.down()
 ring('black', 8)
 
 pen.up()
-pen.setpos(-18, 77)
+pen.goto(-18, 77)
 pen.down()
 ring('white', 4)
 
 pen.up()
-pen.setpos(18, 77)
+pen.goto(18, 77)
 pen.down()
 ring('white', 4)
 
 pen.up()
-pen.setpos(0, 55)
+pen.goto(0, 55)
 pen.down
 ring('black', 5)
 
 pen.up()
-pen.setpos(0, 55)
+pen.goto(0, 55)
 pen.down()
 pen.right(90)
 pen.circle(5, 180)
 pen.up()
-pen.setpos(0, 55)
+pen.goto(0, 55)
 pen.down()
 pen.left(360)
 pen.circle(5, -180)
